006a6720fc5de3c33b3885fc687ce2749e4db97f
nibazshab <44338441+nibazshab@users.noreply.github.com>
2025-05-17 00:08:12 +0800

    core method <- comments

--- core.go
+++ core.go
@@ -11,34 +11,41 @@ type (
 	// Model 帖子版块, Deep: 1 游客可以查看, 2 游客不可查看, 3 游客可以发帖
 	Model struct {
 		Id   int    `gorm:"primaryKey" json:"id"`
-		Name string `json:"name"`
-		Deep int8   `gorm:"default:1" json:"deep"`
+		Name string `gorm:"not null"   json:"name"`
+		Deep int8   `gorm:"default:1"  json:"deep"`
 	}
 
 	// Topic 帖子主题
 	Topic struct {
-		Id        int       `gorm:"primaryKey"`
+		Id        int       `gorm:"primaryKey"      json:"id"`
 		CreatedAt time.Time `gorm:"autoCreateTime"`
-		Title     string    `json:"title"`
-		ModelId   int       `gorm:"index" json:"model_id"`
+		Title     string    `gorm:"not null"        json:"title"`
+		ModelId   int       `gorm:"index;default:0" json:"model_id"`
 		Floors    int       `gorm:"default:1"`
 	}
 
 	// Post 帖子楼层
 	Post struct {
 		Id        int       `gorm:"primaryKey"`
-		TopicId   int       `gorm:"index" json:"topic_id"`
-		Floor     int       `gorm:"index" json:"floor"`
+		TopicId   int       `gorm:"index;not null" json:"topic_id"`
+		Floor     int       `gorm:"index;not null" json:"floor"`
 		UpdatedAt time.Time `gorm:"autoUpdateTime"`
-		Content   string    `json:"content"`
+		Content   string    `gorm:"not null"       json:"content"`
 	}
 )
 
+//	var m = Model{
+//		Name: "",
+//		Deep: 0,
+//	}
 func (m *Model) create() error {
 	// INSERT INTO `models` (`name`,`deep`) VALUES ("测试",1) RETURNING `id`
 	return db.Create(m).Error
 }
 
+//	var m = Model{
+//		Id:   0,
+//	}
 func (m *Model) delete() error {
 	// DELETE FROM `models` WHERE `models`.`id` = 1
 	return db.Delete(m).Error
@@ -49,6 +56,15 @@ func (m *Model) BeforeDelete(tx *gorm.DB) error {
 	return tx.Model(&Topic{}).Where("model_id = ?", m.Id).UpdateColumn("model_id", 0).Error
 }
 
+//	var m = Model{
+//		Id:   0,
+//	}
+//
+//	var m = Model{
+//		Id:   0,
+//		Name: "",
+//		Deep: 0,
+//	}
 func (m *Model) update(new *Model) error {
 	// UPDATE `models` SET `id`=11,`name`="dd" WHERE `id` = 1
 	return db.Set("newId", new.Id).Model(m).Updates(new).Error
@@ -73,6 +89,9 @@ func (m *Model) BeforeUpdate(tx *gorm.DB) error {
 	return tx.Model(&Topic{}).Where("model_id = ?", m.Id).UpdateColumn("model_id", n).Error
 }
 
+//	var m = Model{
+//		Id:   0,
+//	}
 func (m *Model) verifyExist() error {
 	var count int64
 
@@ -87,6 +106,14 @@ func (m *Model) verifyExist() error {
 	return nil
 }
 
+//	var t = Topic{
+//		Title:     "",
+//		ModelId:   0,
+//	}
+//
+//	var p = Post{
+//		Content:   "",
+//	}
 func (t *Topic) create(p *Post) error {
 	// INSERT INTO `topics` (`created_at`,`title`,`model_id`,`floors`)
 	// VALUES ("2025-05-16 00:31:07.555","test",1,1) RETURNING `id`
@@ -119,6 +146,9 @@ func (t *Topic) AfterCreate(tx *gorm.DB) error {
 	return tx.Create(p).Error
 }
 
+//	var t = Topic{
+//		Id:        0,
+//	}
 func (t *Topic) delete() error {
 	// DELETE FROM `topics` WHERE `topics`.`id` = 2
 	return db.Delete(t).Error
@@ -129,6 +159,14 @@ func (t *Topic) BeforeDelete(tx *gorm.DB) error {
 	return tx.Model(&Post{}).Where("topic_id = ?", t.Id).Delete(&Post{}).Error
 }
 
+//	var t = Topic{
+//		Id:        0,
+//	}
+//
+//	var t = Topic{
+//		Title:     "",
+//		ModelId:   0,
+//	}
 func (t *Topic) update(new *Topic) error {
 	// UPDATE `topics` SET `title`="test2",`model_id`=2 WHERE `id` = 2
 	return db.Set("newModelId", new.ModelId).Model(t).Updates(new).Error
@@ -155,11 +193,18 @@ func (t *Topic) BeforeUpdate(tx *gorm.DB) error {
 	return m.verifyExist()
 }
 
+//	var t = Topic{
+//		Id:        0,
+//	}
 func (t *Topic) countFloor() error {
 	// SELECT `floors` FROM `topics` WHERE `topics`.`id` = 2 ORDER BY `topics`.`id` LIMIT 1
 	return db.Select("floors").First(t).Error
 }
 
+//	var p = Post{
+//		TopicId:   0,
+//		Content:   "",
+//	}
 func (p *Post) additional() error {
 	t := Topic{
 		Id: p.TopicId,
@@ -184,11 +229,20 @@ func (p *Post) additional() error {
 	})
 }
 
+//	var p = Post{
+//		TopicId:   0,
+//		Floor:     0,
+//	}
 func (p *Post) delete() error {
 	// DELETE FROM `posts` WHERE topic_id = 2 AND floor = 6
 	return db.Model(p).Where("topic_id = ?", p.TopicId).Where("floor = ?", p.Floor).Delete(p).Error
 }
 
+//	var p = Post{
+//		TopicId:   0,
+//		Floor:     0,
+//		Content:   "",
+//	}
 func (p *Post) update() error {
 	// UPDATE `posts` SET `updated_at`="2025-05-16 18:33:31.041",`content`="" WHERE topic_id = 2 AND floor = 6
 	return db.Model(p).Where("topic_id = ?", p.TopicId).Where("floor = ?", p.Floor).
